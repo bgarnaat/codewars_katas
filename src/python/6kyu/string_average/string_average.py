a11eff5ec13ffd29957364ebfceaff3f18dea4cc
= <garnaat.ben@gmail.com>
2017-07-15 20:26:11 -0700

    add string average

---
+++ src/python/6kyu/string_average/string_average.py
@@ -0,0 +1,50 @@
+"""
+Description:
+
+
+You are given a string of numbers between 0-9. Find the average of these numbers and return it as a floored whole number (ie: no decimal places) written out as a string. Eg:
+
+"zero nine five two" -> "four"
+
+If the string is empty or includes a number greater than 9, return "n/a"
+"""
+
+
+num_dict = {
+    'zero': 0,
+    'one': 1,
+    'two': 2,
+    'three': 3,
+    'four': 4,
+    'five': 5,
+    'six': 6,
+    'seven': 7,
+    'eight': 8,
+    'nine': 9,
+    0: 'zero',
+    1: 'one',
+    2: 'two',
+    3: 'three',
+    4: 'four',
+    5: 'five',
+    6: 'six',
+    7: 'seven',
+    8: 'eight',
+    9: 'nine',
+}
+
+
+def average_string(s):
+    try:
+        s = [num_dict[x] for x in s.split()]
+        return num_dict[sum(s) // len(s)]
+    except (KeyError, ZeroDivisionError):
+        return 'n/a'
+
+
+"""
+BEST ANSWER:
+
+
+
+"""
