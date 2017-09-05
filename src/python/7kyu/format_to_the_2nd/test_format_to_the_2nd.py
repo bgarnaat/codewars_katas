0641dc9e1d34ee4c441daa5d328d5dae98dbb371
= <garnaat.ben@gmail.com>
2017-09-04 17:21:04 -0700

    add travis.yml.  remove extra supermarket_queue.  fix tests

--- src/python/7kyu/format_to_the_2nd/test_format_to_the_2nd.py
+++ src/python/7kyu/format_to_the_2nd/test_format_to_the_2nd.py
@@ -10,17 +10,21 @@ test.assert_equals(print_nums(1, 12, 34), '01\n12\n34')
 test.assert_equals(print_nums(1009, 2), '1009\n0002')
 """
 
-
-# THESE TESTS NEED TO BE WRITTEN WITHOUT ().  also, try to pass allll the params through...
-TEST_LIST = [
-    [(), ''],
-    [(2), '2'],
-    [(1, 12, 34), '01\n12\n34'],
-    [(1009, 2), '1009\n0002'],
-]
+def test_print_nums_no_input():
+    from format_to_the_2nd import print_nums
+    assert print_nums() == ''
 
 
-@pytest.mark.parametrize('input, output')
-def test_print_nums(input, output):
-    from format_to_the_2nd import test_nums
-    assert test_nums(input) == output
+def test_print_nums_one_input():
+    from format_to_the_2nd import print_nums
+    assert print_nums(2) == '2'
+
+
+def test_print_nums_two_input():
+    from format_to_the_2nd import print_nums
+    assert print_nums(1009, 2) == '1009\n0002'
+
+
+def test_print_nums_three_input():
+    from format_to_the_2nd import print_nums
+    assert print_nums(1, 12, 34) =='01\n12\n34'
