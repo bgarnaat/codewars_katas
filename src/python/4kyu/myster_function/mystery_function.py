deffb986b00651b5ec0040066d926153d1fd8afa
= <garnaat.ben@gmail.com>
2017-09-08 20:41:14 -0700

    add best solution

--- src/python/4kyu/myster_function/mystery_function.py
+++ src/python/4kyu/myster_function/mystery_function.py
@@ -61,5 +61,30 @@ def name_of_mystery():
 BEST SOLUTION:
 
 
+Bit manipulation version:
+-------------------------------------
 
+mystery = lambda n: n ^ (n >> 1)
+def mystery_inv(n):
+    m = n >> 1
+    while m != 0:
+        n ^= m
+        m >>= 1
+    return n
+name_of_mystery = lambda: 'Gray code'
+
+--------------------------------------
+
+def mystery(n):
+    return n ^ (n >> 1)
+
+def mystery_inv(n):
+    mask = n >> 1
+    while mask != 0:
+        n = n ^ mask
+        mask = mask >> 1
+    return n;
+
+def name_of_mystery():
+    return "Gray code"
 """
