d5a01453830e00cb55003eadab19e1742ba2fe4d
= <garnaat.ben@gmail.com>
2017-09-10 18:24:42 -0700

    last digit of a huge number

--- src/python/3kyu/last_digit__huge_number/last_digit_huge_number.py
+++ src/python/3kyu/last_digit__huge_number/last_digit_huge_number.py
@@ -16,3 +16,54 @@ Corner cases: we assume that 0 ^ 0 = 1 and that lastDigit of an empty list equal
 This kata generalizes Last digit of a large number; you may find useful to solve it beforehand.
 """
 
+
+def last_digit(lst):
+    if not lst:
+        return 1
+    if len(lst) is 1:
+        return lst[0] % 10
+    prev = lst[-1]
+    for idx, item in enumerate(lst[1:-1][::-1]):
+        if not item and prev:
+            prev = 0
+        else:
+            prev = pow(item, prev, 4) or 4
+    return pow(lst[0], prev, 10)
+
+
+"""
+BEST SOLUTIONS:
+
+
+
+from functools import reduce
+
+def mod_off(number, mod):
+    return min(number, (number - 2) % mod + 2)
+def pow_mod(exp, base):
+    return mod_off(base, 20) ** mod_off(exp, 4)
+def last_digit(lst):
+    return reduce(pow_mod, lst[::-1], 1) % 10
+
+------------------------------------------------------
+
+from functools import reduce
+
+def _powmod(exp, base):
+    return (1 if exp == 0
+            else base if exp == 1 or base == 0
+            else pow(base, 2 + ((exp - 2) % 20), 100) or 20)
+
+def last_digit(arr):
+    return reduce(_powmod, arr[::-1], 1) % 10
+
+-----------------------------------------------------
+
+def last_digit(lst):
+    dig = lst[-1] if lst else 1
+    b = dig != 0
+    for p in lst[-2::-1]:
+        dig = 1 if p == dig == 0 else pow(p, dig or 20*b, 20)
+        b = (dig or 20*b) == 0 or p != 0
+    return dig % 10
+"""
---
+++ src/python/3kyu/last_digit__huge_number/test_last_digit_huge_number.py
@@ -0,0 +1,45 @@
+"""
+TEST CASES:
+
+
+test.it('Basic tests')
+test_data = [
+    ([], 1),
+    ([0, 0], 1),
+    ([0, 0, 0], 0),
+    ([1, 2], 1),
+    ([3, 4, 5], 1),
+    ([4, 3, 6], 4),
+    ([7, 6, 21], 1),
+    ([12, 30, 21], 6),
+    ([2, 2, 2, 0], 4),
+    ([937640, 767456, 981242], 0),
+    ([123232, 694022, 140249], 6),
+    ([499942, 898102, 846073], 6)
+]
+for test_input, test_output in test_data:
+    test.assert_equals(last_digit(test_input), test_output)
+"""
+import pytest
+
+
+TEST_CASES = [
+    ([], 1),
+    ([0, 0], 1),
+    ([0, 0, 0], 0),
+    ([1, 2], 1),
+    ([3, 4, 5], 1),
+    ([4, 3, 6], 4),
+    ([7, 6, 21], 1),
+    ([12, 30, 21], 6),
+    ([2, 2, 2, 0], 4),
+    ([937640, 767456, 981242], 0),
+    ([123232, 694022, 140249], 6),
+    ([499942, 898102, 846073], 6)
+]
+
+
+@pytest.mark.parametrize('test_input, test_output', TEST_CASES)
+def test_last_digit(test_input, test_output):
+    from last_digit_huge_number import last_digit
+    assert last_digit(test_input) == test_output
